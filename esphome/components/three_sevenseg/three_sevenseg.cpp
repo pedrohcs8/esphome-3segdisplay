043ea6a3e7f454236158afa0c366593d1c2f1786
pedrohcs8 <pedrohenriquecsena@gmail.com>
2026-07-05 17:04:50 -0400

    test13

--- esphome/components/three_sevenseg/three_sevenseg.cpp
+++ esphome/components/three_sevenseg/three_sevenseg.cpp
@@ -239,14 +239,14 @@ void THREE_SEVENSEGComponent::set_digit_(uint8_t digit, uint8_t ch, bool dot) {
     ct++;
   }
 
-  this->dp_pin_->digital_write(!((segments & 0b10000000) || dot));
-  this->a_pin_->digital_write(!(segments & 0b01000000));
-  this->b_pin_->digital_write(!(segments & 0b00100000));
-  this->c_pin_->digital_write(!(segments & 0b00010000));
-  this->d_pin_->digital_write(!(segments & 0b00001000));
-  this->e_pin_->digital_write(!(segments & 0b00000100));
-  this->f_pin_->digital_write(!(segments & 0b00000010));
-  this->g_pin_->digital_write(!(segments & 0b00000001));
+  this->dp_pin_->digital_write((segments & 0b10000000) || dot);
+  this->a_pin_->digital_write(segments & 0b01000000);
+  this->b_pin_->digital_write(segments & 0b00100000);
+  this->c_pin_->digital_write(segments & 0b00010000);
+  this->d_pin_->digital_write(segments & 0b00001000);
+  this->e_pin_->digital_write(segments & 0b00000100);
+  this->f_pin_->digital_write(segments & 0b00000010);
+  this->g_pin_->digital_write(segments & 0b00000001);
 
   delay(5);
 };
