3d0aeb1b975e33eb11c41a23f1a90d6010d006e6
pedrohcs8 <pedrohenriquecsena@gmail.com>
2026-07-05 15:07:49 -0400

    fix2

--- esphome/components/three_sevenseg/three_sevenseg.h
+++ esphome/components/three_sevenseg/three_sevenseg.h
@@ -14,8 +14,6 @@ using three_sevenseg_writer_t = display::DisplayWriter<THREE_SEVENSEGComponent>;
 
 class THREE_SEVENSEGComponent final : public PollingComponent {
  public:
-  explicit THREE_SEVENSEGComponent();
-
   void set_writer(three_sevenseg_writer_t &&writer);
 
   void setup() override;
